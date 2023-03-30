d12fa305cd49ca039d4aca3b34e4e82553639fad
parcoyou <parcoyou@xiaoe-tech.com>
2023-03-30 22:18:15 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  base: "/log-analysis-ui/",
   server: {
     host: "0.0.0.0"
   },
