1b7fba21d297dcde2e0de346d066a8f84100415d
parcoyou <parcoyou@xiaoe-tech.com>
2023-03-30 23:04:36 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import pkg from './package.json'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  base: "/log-analysis-ui/",
+  base: `/${pkg.name}/`,
   server: {
     host: "0.0.0.0"
   },
